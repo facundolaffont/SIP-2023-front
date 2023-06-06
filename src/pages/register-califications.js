c73a5ebb2218aff0de40c4a084207b093ad2ca0b
leoduville <71992403+leoduville@users.noreply.github.com>
2023-06-05 21:08:07 -0300

    registro de calificaciones

--- src/pages/register-califications.js
+++ src/pages/register-califications.js
@@ -10,6 +10,11 @@ export function CalificationRegistering() {
   const [sheetNameValue, setSheetNameValue] = useState('');
   const [cellRangeName, setCellRangeName] = useState('');
   const [calificationDate, setCalificationDate] = useState('');
+  const [spreadsheetManipulator, setSpreadSheet] = useState('');
+  const [firstColumn, setFirstColumn] = useState('Legajo');
+  const [lastColumn, setLastColumn] = useState('Asistencia');
+  const [selectedEventId, setSelectedEventId] = useState(null); // Estado para almacenar el ID del evento seleccionado
+
 
   const loadFile = (event) => {
     // Evita que se ejecute la llamada del submit.
@@ -30,6 +35,7 @@ export function CalificationRegistering() {
     let calificationsTable = document.getElementsByClassName("calification-table")[0];
     spreadsheetManipulator.insertDataIntoTable(calificationsTable, "Tabla de calificaciones");
     calificationsTable.classList.remove("not-displayed");
+    setSpreadSheet(spreadsheetManipulator);
   }
 
   const handleFileSelection = (event) => {
@@ -51,13 +57,126 @@ export function CalificationRegistering() {
   }
 
   const handleCalificationDate = (event) => {
-    setCalificationDate(
-      event.target.value
-    );
-  }
+    const selectedDate = event.target.value;
+    setCalificationDate(selectedDate);
+    
+    fetch(`${process.env.REACT_APP_API_SERVER_URL}/api/v1/events/get?date=${selectedDate}`, {
+      method: 'GET',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los eventos de la cursada");
+        }
+        return response.json();
+      })
+      .then((classEvents) => {
+        // Realiza las operaciones necesarias con los eventos de la cursada obtenidos
+        console.log(classEvents); // Ejemplo: muestra los eventos en la consola
+        const eventosContainer = document.getElementById('eventos-container');
+        const tituloContainer = document.createElement('h2');
+        tituloContainer.textContent = 'Eventos para la fecha';
+        eventosContainer.innerHTML = '';
+  
+        // Iterar sobre las cursadas y crear un cuadro para cada una
+        classEvents.forEach((evento, index) => {
+          const cuadroEvento = document.createElement('div');
+          cuadroEvento.classList.add('cuadro-cursada');
+          // Agregar un atributo de datos para almacenar el índice de la cursada
+          cuadroEvento.setAttribute('data-index', index);
 
-  const handleRegistering = (event) => {
-    // Construir JSON y enviarlo al back a la API
+          // Mostrar los detalles de la cursada dentro del cuadro
+          const nombreEvento = document.createElement('h3');
+          nombreEvento.textContent = `Tipo de evento: ` + evento.tipoEvento.nombre;
+
+          const detallesEvento = document.createElement('p');
+          detallesEvento.textContent = `Fecha de Inicio: ${formatDateTime(evento.fechaHoraInicio)}, Fecha de Fin: ${formatDateTime(evento.fechaHoraFin)}`;
+
+          cuadroEvento.appendChild(nombreEvento);
+          cuadroEvento.appendChild(detallesEvento);
+
+          // Agregar un evento de clic al cuadro de la cursada
+          cuadroEvento.addEventListener('click', () => {
+            // Obtener el índice de la cursada seleccionada
+            const selectedIndex = parseInt(cuadroEvento.getAttribute('data-index'), 10);
+
+            const selectedEventId = classEvents[selectedIndex].id;
+
+            // Verificar si el cuadro ya está seleccionado
+            const isAlreadySelected = cuadroEvento.classList.contains('selected');
+
+            // Resalto visualmente el evento seleccionado o lo deselecciono
+            const cuadrosEventos = document.getElementsByClassName('cuadro-cursada');
+            for (let i = 0; i < cuadrosEventos.length; i++) {
+              cuadrosEventos[i].classList.remove('selected');
+            }
+
+            if (!isAlreadySelected) {
+              // Seteo el evento seleccionado
+              setSelectedEventId(selectedEventId);
+              cuadroEvento.classList.add('selected');
+            } else {
+              // Deseleccionar el evento
+              setSelectedEventId(null);
+            }
+          });
+          eventosContainer.appendChild(cuadroEvento);
+        });
+      })
+      .catch((error) => {
+        // Maneja los errores de la solicitud
+        console.error(error);
+      });
+  }
+    
+  const formatDateTime = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return dateTime.toLocaleString('es-ES', options);
+  };
+
+  const handleRegistering = () => {
+    // Verificar si se ha seleccionado un evento
+    if (selectedEventId) {
+      // Obtener la lista de calificaciones
+      //const calificaciones = obtenerListaDeCalificaciones(); // Debes implementar esta función según tus necesidades
+      const calificacionData = spreadsheetManipulator.get().data.map((item) => {
+        return {
+          studentDossier: item[firstColumn], // Utiliza el valor de 'Legajo' como studentDossier
+          calification: item[lastColumn], // Utiliza el valor de 'Asistencia' como attendance
+        };
+      });
+      console.log(calificacionData);
+      // Crear el objeto de datos a enviar al endpoint
+      const data = {
+        courseEventId: selectedEventId,
+        califications: calificacionData
+      };
+      // Realizar la solicitud al endpoint para registrar la asistencia
+      fetch(`${process.env.REACT_APP_API_SERVER_URL}/api/v1/events/add-califications-on-event`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al registrar la calificacion");
+        }
+        return response.json();
+      })
+      .then((result) => {
+        // Realizar las operaciones necesarias con el resultado de la solicitud
+        console.log(result);
+      })
+      .catch((error) => {
+        // Manejar los errores de la solicitud
+        console.error(error);
+      });
+    } else {
+      // Mostrar un mensaje de error o realizar alguna acción en caso de no haber seleccionado un evento
+      console.error("No se ha seleccionado un evento");
+    }
   }
 
   return (
@@ -99,6 +218,7 @@ export function CalificationRegistering() {
           className="register-calification-button"
           onClick={handleRegistering}
         >Registrar calificaciones</button>
+        <div id="eventos-container"></div>
       </form>
       <table className="calification-table not-displayed">
       </table>
