50183bc9bd7bd8927b456f32a1cb3c0d4adc0531
Facundo Laffont <facundol@mac-osx.local>
2023-06-19 01:48:10 -0300

    Configuración del pipeline

--- push-image-to-dockerhub.sh
+++ push-image-to-dockerhub.sh
@@ -1,7 +1,6 @@
-# Construye la app, la imagen de Docker y la sube al repositorio de Docker Hub.
+# Construye la app y la sube al repositorio de Docker Hub.
 
 set -e
 
-npm build
 docker build -t facundol/sip-frontend:latest .
 docker push facundol/sip-frontend:latest
