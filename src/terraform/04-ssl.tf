d4488c7ac9db6c514f1dee1d37fe14354b27e9ca
Facundo Laffont <facundol@mac-osx.local>
2023-06-19 14:44:45 -0300

    [ALL-UP]

---
+++ src/terraform/04-ssl.tf
@@ -0,0 +1,10 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_managed_ssl_certificate.html
+resource "google_compute_managed_ssl_certificate" "load_balancer_cert" {
+  provider = google-beta
+  name     = "front-lb-ssl-cert"
+
+  managed {
+    domains = [data.cloudflare_zone.spgda]
+  }
+
+}
