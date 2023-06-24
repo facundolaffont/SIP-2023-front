f8aaf6d63a7df9f4a35b4a1a0ea09fe709186339
leoduville <71992403+leoduville@users.noreply.github.com>
2023-06-23 21:10:14 -0300

    ada

--- src/pages/final-condition.js
+++ src/pages/final-condition.js
@@ -3,15 +3,15 @@ import { PageLayout } from "../components/page-layout";
 export function FinalCondition() {
 
   const handleSubmit = (event) => {
+    event.preventDefault();
 
     // Enviamos petición al back para calcular condicion final de los alumnos de la cursada
 
-    fetch(`${process.env.REACT_APP_API_SERVER_URL}/api/v1/course/finalCondition`, {
-        method: "POST",
+    fetch(`${process.env.REACT_APP_API_SERVER_URL}/api/v1/course/finalCondition?courseId=1`, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({courseId:1})
       })
         .then((response) => response.json())
         .then((data) => console.log(data))
@@ -23,7 +23,7 @@ export function FinalCondition() {
     <PageLayout>
     <h1 id="page-title" className="content__title">Condición Final</h1>
     <form onSubmit={handleSubmit}>
-    <button type="submit">Calcular Condicion Final de Alumnos</button>
+    <button type="submit" >Calcular Condicion Final de Alumnos</button>
     </form>
     </PageLayout>
   );
